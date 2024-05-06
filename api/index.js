1a95d395f3ee603bbab49683e04a22700142543e
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-05-06 13:04:24 -0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from "express";
-const app=express();
-const PORT=3000;
-app.listen(PORT,()=>
-{
-    console.log(`Server Listening on ${PORT}`);
-    
-})
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to db!");
+  })
+  .catch((err) => {
+    console.log("error connecting to db!" + err);
+  });
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server Listening on ${PORT}`);
+});
