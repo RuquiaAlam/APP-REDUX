4cc81c3c3a685363a6efce899d4e462f3ebba5ae
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-05-07 10:17:44 -0500

    added errorHandler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   // console.log(req.body);
 
   const { username, email, password } = req.body;
@@ -12,9 +13,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
     console.log(req.body);
-  } catch (err) {
-    res.status(500).json({
-      message: err.message,
-    });
+  } catch (error) {
+    next(error);
   }
 };
