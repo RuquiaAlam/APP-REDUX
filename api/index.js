bfc53434e20a81f92ac9369cf095f2cf857850b9
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-05-08 00:23:54 -0500

    complete signup page functionality

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.route.js"
-import authRoute from "./routes/auth.route.js"
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -20,16 +20,15 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server Listening on ${PORT}`);
 });
-// 
-app.use("/api/user",userRoute)
-app.use("/api/auth",authRoute);
-app.use((err,req,res,next)=>
-{
-    const statusCode=err.statusCode||500;
-    const message=err.message||'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        error:message,
-        statusCode:statusCode
-    })
-})
+//
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    error: message,
+    statusCode: statusCode,
+  });
+});
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,24 +3,41 @@ import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-  console.log(formData);
+  // console.log(formData);
   const handleSubmit = async (e) => {
     //prevents refreshing the page
     e.preventDefault();
-    const res = await fetch("/api/auth/signup", {
-      method:'POST',
-      headers:{
-        'Content-Type':"application/json",
-
-      },
-      body:JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      
+      console.log(data);
+      setLoading(false);
+        if (data.success===false) {
+        setError(true);
+        return;
+        }
+        setError(false);
+   
+    } catch (error) {
+      setLoading(false);
+      
+    setError(true);
+      
+    }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -47,8 +64,11 @@ export default function SignUp() {
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase disabled:opacity-80 hover:opacity-95">
-          SignUp
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase disabled:opacity-80 hover:opacity-95"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 m-5">
@@ -57,6 +77,7 @@ export default function SignUp() {
           <span className="text-blue-600">Sign In </span>
         </Link>
       </div>
+      <p className="text-red-700 m-5 ">{error && "Something went wrong!"}</p>
     </div>
   );
 }
